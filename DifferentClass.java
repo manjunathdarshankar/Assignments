6e16cf925a1646a90568594d9e8aedb5754f5df8
manjunathdarshankar <manjunathdarshankar@gmail.com>
2022-03-14 13:33:14 +0530

    new file

---
+++ DifferentClass.java
@@ -0,0 +1,25 @@
+package com.te.javabasics.accessmodifiers;
+
+import com.te.javabasics.inheritance.InSameClass;
+
+public class DifferentClass {
+
+	public void publicMethod() {
+		System.out.println("This is a public method");
+	}
+
+	public void protectedMethod() {
+		System.out.println("This is a protected method");
+	}
+
+	void defaultMethod() {
+		System.out.println("This is a default method");
+	}
+
+	public static void main(String[] args) {
+		DifferentClass class1 = new DifferentClass();
+		class1.defaultMethod();
+		class1.protectedMethod();
+		class1.publicMethod();
+	}
+}
